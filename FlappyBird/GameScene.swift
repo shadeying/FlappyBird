1c613c8e8bfda641ea42bfc043859b75246664c9
Shade Wong <shadeying@gmail.com>
2020-06-09 22:27:31 -0700

    Initial commit

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -7,83 +7,59 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var flappybird: SKSpriteNode!
+    var sinceTouch : CFTimeInterval = 0 // keep track of touch time to turn the flappybird
+    let fixedDelta: CFTimeInterval = 1.0 / 60.0 // 60 FPS
     
     override func didMove(to view: SKView) {
+        /* Set up your scene here */
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        /* Recursive node search for 'flappybird' (child of referenced node) */
+        flappybird = self.childNode(withName: "//flappybird") as? SKSpriteNode
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+       /* Called when a touch begins */
+        
+        /* Apply vertical impulse */
+        flappybird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 30))
+        
+        /* Apply subtle rotation */
+        flappybird.physicsBody?.applyAngularImpulse(1)
+
+        /* Reset touch timer */
+        sinceTouch = 0
+        
+        /* Play SFX */
+        let flapSFX = SKAction.playSoundFileNamed("sfx_flap", waitForCompletion: false)
+        self.run(flapSFX)
+    }
+
+    override func update(_ currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        
+        /* Grab current velocity */
+        let velocityY = flappybird.physicsBody?.velocity.dy ?? 0
+
+        /* Check and cap vertical velocity */
+        if velocityY > 400 {
+          flappybird.physicsBody?.velocity.dy = 400
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        /* Apply falling rotation */
+        if sinceTouch > 0.1 {
+            let impulse = -20000 * fixedDelta
+            flappybird.physicsBody?.applyAngularImpulse(CGFloat(impulse))
+        }
+
+        /* Clamp rotation */
+        let _ = flappybird.zRotation.clamp(v1: CGFloat(-20).degreesToRadians(),CGFloat(40).degreesToRadians())
+        let _ = flappybird.physicsBody?.angularVelocity.clamp(v1: -2, 2)
+
+        /* Update last touch timer */
+        sinceTouch += fixedDelta
     }
 }
